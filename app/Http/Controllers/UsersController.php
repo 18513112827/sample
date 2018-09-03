9c97d7125441dd41ed19b1db8f89a49184e43638
append <append0594@163.com>
2018-09-03 08:32:22 +0000

    finish user edit, update, index and destroy action

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,7 +13,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth',[
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store', 'index']
         ]);
 
         $this->middleware('guest', [
@@ -77,5 +77,19 @@ class UsersController extends Controller
 
         session()->flash('success', '恭喜你，修改成功！');
         return redirect()->route('users.show', [$user]);
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
     }
 }
